1214a76fd4ea99502b802a57cd7c75ac2db0ff91
SudhirAjodhya <sudhir.ajodhya@gmail.com>
2025-09-09 16:28:45 +0200

    feat: updated model for Badges collection

--- src/app/db/models/MBadge.model.ts
+++ src/app/db/models/MBadge.model.ts
@@ -6,13 +6,15 @@ export interface IBadgeDocument extends Document, IBadge{}
 const badgeSchema = new Schema<IBadgeDocument>({
     name: { type: String, required: true, unique: true, trim: true},
     image: { type: String, required: true },
-    TLA: { type: String, required: true, unique: true, trim: true },
-    summary: { type: String, required: true, unique: true},
+    TLA: { type: String, required: true, trim: true },
+    summary: { type: String, required: true },
     description: { type: String, required: true },
     permanent: { type: Boolean, required: true, default: false },
     expirationDate: { type: Date, required: false },
-    Bonus: { type: mongoose.Schema.Types.Decimal128, required: true, default: 0 }
+    bonus: { type: mongoose.Schema.Types.Decimal128, required: true, default: 0 }
     
 }, { timestamps: true })
 
 const MBadge = model<IBadgeDocument>("Badges", badgeSchema); 
+
+export default MBadge;
