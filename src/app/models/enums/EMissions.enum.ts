e266da3be0cb889823716e5f03deac3de57ea7e5
MayB-04 <maybadenhorst@gmail.com>
2025-09-17 17:35:43 +0200

    feat(missionsAPI):aquick enum change

--- src/app/models/enums/EMissions.enum.ts
+++ src/app/models/enums/EMissions.enum.ts
@@ -1,11 +1,4 @@
-/**
- * Represents the various states a bundle can be in,
- * from creation to approval.
- * 0 = Active
- * 1 = Completed
- * 2 = Achieved
- * 3 = Failed
- */
+
 export enum EMissionStatus {
     Active = 'active',
     InActive = 'inactive',
