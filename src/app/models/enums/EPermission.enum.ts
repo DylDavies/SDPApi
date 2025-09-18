dda611a184eebd45bbce40b2b566c2067e782b60
SudhirAjodhya <sudhir.ajodhya@gmail.com>
2025-09-18 20:46:41 +0200

    feat: added permissions for badge system

--- src/app/models/enums/EPermission.enum.ts
+++ src/app/models/enums/EPermission.enum.ts
@@ -39,5 +39,7 @@ export enum EPermission {
     BADGES_CREATE = 'badges:create',
     BADGES_MANAGE = 'badges:manage',
     BADGES_VIEW = 'badges:view',
-
+    BADGES_VIEW_REQUIREMENTS = 'badges:view_requirements',
+    BADGES_MANAGE_REQUIREMENTS = 'badges:manage_requirements',
+ 
 }
